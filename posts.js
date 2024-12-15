eaab40d85db2db816bd2ed56cd1314976ef27ff5
Coboltplayz <90228566+Coboltplayz@users.noreply.github.com>
2024-12-16 06:53:09 +0800

    test

---
+++ posts.js
@@ -0,0 +1,24 @@
+localStorage.setItem("facebookPosts", JSON.stringify([
+    {
+        "title": "UP Mindanao Kasadya Celebration 🎄",
+        "content": `Maayong hapon, UP Mindanao! 🌟 
+    Catch our booth later at the UP Mindanao Atrium during this year’s Kasadya celebration! 🎄
+    For only 10 pesos, you get to choose any song and play a game of Guitar Hero! 🤯 We also have AMPLI STICKERS available. Limited stocks only so buy now! 😱
+    So, what are you waiting for? Head on to our booth and celebrate the Kasadya spirit with us! ✨
+    See you there! 😎🫵🏻
+    
+    #Kasadya2024 #1-2425 #UPMinAMPLI #UPMindanao`,
+        "image": "https://scontent.fdvo2-2.fna.fbcdn.net/v/t39.30808-6/469537345_994717366009738_13396572978214086_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=qWUK1kxx_6UQ7kNvgFff1hG&_nc_zt=23&_nc_ht=scontent.fdvo2-2.fna&_nc_gid=A51njBID2p0yx9hYg6yWYxu&oh=00_AYAlg_VwygwfQRRVmVN_N2rGEOoZsvq5aqplW4YtkNgEEw&oe=67654AC0",
+        "link": "https://www.facebook.com/share/p/1EZKSZ36ZP/"
+    },
+    {
+      "title": "Post Title 2",
+      "content": `Hey AMPLI fam! Sharing some highlights from the recently concluded Bonifacio Day Cultural Night organized by the USC and Anakbayan UP Mindanao, as well as the Gabriela Youth Benefit Gig "Himig at Tula ng Paglaya". UP Mindanao AMPLI proudly participated as performers in both events. We’re deeply grateful for the opportunity to collaborate with these organizations. Here’s to more gigs and partnerships in the upcoming academic semester!
+    Photos by: Ella 
+    Captions: Ella 
+    Pubmat: Jiane and Nezzie
+    #supportlocalmusic #UPMINAMPLI24 #gig #OPM`,
+      "image": "https://scontent.fdvo2-2.fna.fbcdn.net/v/t39.30808-6/468926460_993043619510446_710345629760188498_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=RPfzJnMnXVkQ7kNvgGrA_ma&_nc_zt=23&_nc_ht=scontent.fdvo2-2.fna&_nc_gid=AjTkfvsFv1eUmrwwMT7VOi4&oh=00_AYDF1w9bvDBXmG9yHAjfV0Z_BSgM87_7KcrBdpEUKqtQ5w&oe=67654B69",
+      "link": "https://www.facebook.com/share/p/14sK7y6kKr/"
+    }
+  ]));
